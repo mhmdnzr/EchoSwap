2281c7d59583a3f905755fc78f23b56576c0f972
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-12-14 15:21:06 +0330

    projectid

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    supportFile: 'cypress/support/e2e.ts',
+    supportFile: "cypress/support/e2e.ts",
   },
-
+  projectId: "f9bdyg",
   component: {
     devServer: {
       framework: "next",
